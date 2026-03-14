1baf00818e1dc7ec972bea055f9e9ee91133216f
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2026-03-14 20:24:13 +0000

    add new secret

--- modules/compute/ssh.tf
+++ modules/compute/ssh.tf
@@ -21,3 +21,13 @@ resource "google_secret_manager_regional_secret_version" "ssh_public_key_openssh
   secret      = google_secret_manager_regional_secret.ssh_public_key.id
   secret_data = tls_private_key.private_key.public_key_openssh
 }
+
+resource "google_secret_manager_regional_secret" "ip_address" {
+  secret_id = "${var.instance_name}-ip-address"
+  location  = var.region
+}
+
+resource "google_secret_manager_regional_secret_version" "ip_address" {
+  secret      = google_secret_manager_regional_secret.ip_address.id
+  secret_data = google_compute_instance.compute_instance.network_interface.0.access_config.0.nat_ip
+}
